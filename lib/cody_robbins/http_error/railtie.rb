c3f666f3f1053f33064e9f2f200d1094eabf0e85
Cody Robbins <cody@sakuzaku.com>
2011-05-20 17:39:26 -0400

    The railtie initializer now properly fires and includes the correct module.

--- lib/cody_robbins/http_error/railtie.rb
+++ lib/cody_robbins/http_error/railtie.rb
@@ -3,7 +3,7 @@ module CodyRobbins
     class Railtie < Rails::Railtie
       initializer('cody_robbins.http_error') do
         ActiveSupport.on_load(:action_controller) do
-          ApplicationController.send(:include, HttpError)
+          ApplicationController.send(:include, InstanceMethods)
         end
       end
     end

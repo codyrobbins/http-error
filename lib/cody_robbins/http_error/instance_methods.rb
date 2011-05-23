347ae7c71051eaf3709660c58a9115485152dcb2
Cody Robbins <cody@sakuzaku.com>
2011-05-23 18:13:39 -0400

    Added some documentation.

--- lib/cody_robbins/http_error/instance_methods.rb
+++ lib/cody_robbins/http_error/instance_methods.rb
@@ -1,8 +1,30 @@
+# encoding: UTF-8
+
 module CodyRobbins
   module HttpError
     module InstanceMethods
       protected
 
+      # Returns the specified HTTP code in the action’s response, renders the correspondingly named HTML error document in `public`, and returns `false`. If a corresponding HTML error document doesn’t exist, you’ll probably get some sort of exception.
+      #
+      # @param code [Integer] The HTTP status code to return—for example, 404.
+      # @return [boolean] Returns `false` in order to halt any filter chain it may be invoked from.
+      #
+      # @example The following will return a 404 HTTP code, render `public/404.html`, and halt the filter chain so that `@user.destroy!` is never executed.
+      #    class UserController < ApplicationController
+      #      before_filter(:get_user)
+      #
+      #      def delete
+      #        @user.destroy
+      #      end
+      #
+      #      protected
+      #
+      #      def get_user
+      #        @user = User.find_by_id(params[:id])
+      #        http_error(404) unless @user
+      #      end
+      #    end
       def http_error(code)
         render(:file => Rails.root.join('public', "#{code}.html"),
                :layout => false,

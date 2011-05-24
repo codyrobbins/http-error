04427507891f2ec1a42c53c404a5071f006fbdf1
Cody Robbins <cody@sakuzaku.com>
2011-05-24 16:17:44 -0400

    Fixed the railtie so that it injects the instance method into ApplicationController so that it won't be lost across requests in development mode.

--- lib/cody_robbins/http_error/railtie.rb
+++ lib/cody_robbins/http_error/railtie.rb
@@ -1,9 +1,11 @@
+require('rails')
+
 module CodyRobbins
   module HttpError
     class Railtie < Rails::Railtie
       initializer('cody_robbins.http_error') do
         ActiveSupport.on_load(:action_controller) do
-          ApplicationController.send(:include, InstanceMethods)
+          include(InstanceMethods)
         end
       end
     end
